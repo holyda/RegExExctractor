f359520ff044677035847e3ad1af557cbdd318ec
Daniel <you@example.comdanilka20009@gmail.com>
2021-03-23 12:44:15 +0300

    Version 0.0.1 without interface

---
+++ EmailPhNumExtractor.py
@@ -0,0 +1,37 @@
+import pyperclip, re
+
+phoneRegEx = re.compile(r'''(
+	(\d{3}|\(\d{3}\))?				# area code
+	(\s|-|\.)?						# separator
+	(\d{3})							# first three digits
+	(\s|-|\.)						# separator
+	(\d{4})							# last four digits
+	(\s*(ext|x|ext.)\s*(\d{2,5}))?	# extension
+	)''', re.VERBOSE)
+
+# TODO: Create email regex
+emailRegEx = re.compile(r'''(
+	[a-zA-Z0-9._%+-]+		# username
+	@						# @ symbol
+	[a-zA-Z0-9.-]+			# domain name
+	(\.[a-zA-Z]{2,4})		# dot-something
+	)''', re.VERBOSE)
+# TODO: Find matches in clipboard text
+text = str(pyperclip.paste())
+
+matches = []
+for groups in phoneRegEx.findall(text):
+	phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+	if groups[8]!= '':
+		phoneNum += ' x' + groups[8]
+	matches.append(phoneNum)
+for groups in emailRegEx.findall(text):
+	matches.append(groups[0])
+
+# TODO: Copy results to the clipboard
+if len(matches) > 0:
+	pyperclip.copy('\n'.join(matches))
+	print('Copied to clipboard:')
+	print('\n'.join(matches))
+else:
+	print('No phone numbers or email addresses found :(')
